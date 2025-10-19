265836659785a67bb3c9ac6e83f3425c609ad921
ARMINION <girondopr@gmail.com>
2025-10-19 04:23:20 +0200

    vuelta al ruedo primer dia, poniendome al dia con java

--- src/main/java/com/swingy/App.java
+++ src/main/java/com/swingy/App.java
@@ -1,13 +1,11 @@
 package com.swingy;
 
-/**
- * Hello world!
- *
- */
+import com.swingy.controller.*;;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        controller.start();
     }
 }
--- src/main/java/com/swingy/controller/controller.java
+++ src/main/java/com/swingy/controller/controller.java
@@ -1,8 +1,41 @@
 package com.swingy.controller;
 
-import com.swingy.model.model;
-import com.swingy.view.view;
+import jakarta.validation.constraints.Pattern;
 
 public class controller {
+
+    private controller()
+    {
+
+    }
+
+    public static void start()
+    {
+        create_character();
+        
+        while(true){}
+    }
+
+    private static void create_character()
+    {
+        System.out.print("\033[H\033[2J"); // limpia la terminal
+
+        System.out.println( "\nWelcome to Swingy !!\n" );
+        System.out.println( "Introduce your name: " );
+
+        @Pattern(regexp = "^[\\p{L}]+$", message = "Solo letras") // valida input solo letras
+        String c_name = System.console().readLine();
+        
+        System.out.print("\033[H\033[2J");
+
+        System.out.println( "\nChoose a class !!\n" );
+        System.out.println( "1) Mage  2) Warrior 3) Archer" );
+        String c_class = System.console().readLine();
+
+        System.out.print("\033[H\033[2J");
+        System.out.println( "\n" + c_name + ": " + "\n" );
+        System.out.println( "      -Class" + ": " + c_class + "\n" );
+
+    }
     
 }
